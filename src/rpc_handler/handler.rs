1aff3bebbd67274f2d32c5c8676a9de9c0487bfd
lovedret <lovedret@6block.com>
2024-01-14 00:50:31 +0800

    Add rpc error handler

--- src/rpc_handler/handler.rs
+++ src/rpc_handler/handler.rs
@@ -1,11 +1,11 @@
 use std::time::Duration;
 
-use anyhow::Result;
-use ureq::{Agent, AgentBuilder};
+use serde::Deserialize;
+use ureq::{Agent, AgentBuilder, Error, Response};
 
 use crate::web_handlers::abi::{GetAccountStatusRep, GetAccountStatusReq, GetLatestBlockRep};
 
-use super::abi::*;
+use super::{abi::*, RpcError};
 
 #[derive(Debug, Clone)]
 pub struct RpcHandler {
@@ -24,82 +24,90 @@ impl RpcHandler {
         }
     }
 
-    pub async fn import_view_only(&self, req: ImportAccountReq) -> Result<ImportAccountRep> {
-        let path = format!("http://{}/wallet/importAccount", self.endpoint);
-        let resp: RpcResponse<ImportAccountRep> = self
-            .agent
-            .clone()
-            .post(&path)
-            .send_json(ureq::json!({"account": req}))?
-            .into_json()?;
-        Ok(resp.data)
+    pub fn handle_response<S: for<'a> Deserialize<'a>>(
+        &self,
+        resp: Result<Response, Error>,
+    ) -> Result<S, RpcError> {
+        match resp {
+            Ok(response) => match response.into_json::<RpcResponse<S>>() {
+                Ok(data) => Ok(data.data),
+                Err(e) => Err(RpcError {
+                    code: "Unknown".into(),
+                    status: 606,
+                    message: e.to_string(),
+                }),
+            },
+            Err(ureq::Error::Status(_code, response)) => match response.into_json::<RpcError>() {
+                Ok(data) => Err(data),
+                Err(e) => Err(RpcError {
+                    code: "Unknown".into(),
+                    status: 606,
+                    message: e.to_string(),
+                }),
+            },
+            Err(e) => Err(RpcError {
+                code: "Unknown".into(),
+                status: 606,
+                message: "e".to_string(),
+            }),
+        }
     }
 
-    pub async fn get_balance(&self, req: GetBalancesReq) -> Result<GetBalancesRep> {
-        let path = format!("http://{}/wallet/getBalances", self.endpoint);
-        let resp: RpcResponse<GetBalancesRep> = self
+    pub async fn import_view_only(
+        &self,
+        req: ImportAccountReq,
+    ) -> Result<ImportAccountRep, RpcError> {
+        let path = format!("http://{}/wallet/importAccount", self.endpoint);
+        let resp = self
             .agent
             .clone()
             .post(&path)
-            .send_json(ureq::json!(req))?
-            .into_json()?;
-        Ok(resp.data)
+            .send_json(ureq::json!({"account": req}));
+        self.handle_response(resp)
+    }
+
+    pub async fn get_balance(&self, req: GetBalancesReq) -> Result<GetBalancesRep, RpcError> {
+        let path = format!("http://{}/wallet/getBalances", self.endpoint);
+        let resp = self.agent.clone().post(&path).send_json(&req);
+        self.handle_response(resp)
     }
 
     pub async fn get_transactions(
         &self,
         req: GetTransactionsReq,
-    ) -> Result<Vec<TransactionStatus>> {
+    ) -> Result<Vec<TransactionStatus>, RpcError> {
         let path = format!("http://{}/wallet/getAccountTransactions", self.endpoint);
-        let resp: RpcResponse<Vec<TransactionStatus>> = self
-            .agent
-            .clone()
-            .post(&path)
-            .send_json(ureq::json!(req))?
-            .into_json()?;
-        Ok(resp.data)
+        let resp = self.agent.clone().post(&path).send_json(&req);
+        self.handle_response(resp)
     }
 
-    pub async fn create_transaction(&self, req: CreateTxReq) -> Result<CreateTxRep> {
+    pub async fn create_transaction(&self, req: CreateTxReq) -> Result<CreateTxRep, RpcError> {
         let path = format!("http://{}/wallet/createTransaction", self.endpoint);
-        let resp: RpcResponse<CreateTxRep> = self
-            .agent
-            .clone()
-            .post(&path)
-            .send_json(ureq::json!(req))?
-            .into_json()?;
-        Ok(resp.data)
+        let resp = self.agent.clone().post(&path).send_json(&req);
+        self.handle_response(resp)
     }
 
-    pub async fn broadcast_transaction(&self, req: BroadcastTxReq) -> Result<BroadcastTxRep> {
+    pub async fn broadcast_transaction(
+        &self,
+        req: BroadcastTxReq,
+    ) -> Result<BroadcastTxRep, RpcError> {
         let path = format!("http://{}/chain/broadcastTransaction", self.endpoint);
-        let resp: RpcResponse<BroadcastTxRep> = self
-            .agent
-            .clone()
-            .post(&path)
-            .send_json(ureq::json!(req))?
-            .into_json()?;
-        Ok(resp.data)
+        let resp = self.agent.clone().post(&path).send_json(&req);
+        self.handle_response(resp)
     }
 
     pub async fn get_account_status(
         &self,
         req: GetAccountStatusReq,
-    ) -> Result<GetAccountStatusRep> {
+    ) -> Result<GetAccountStatusRep, RpcError> {
         let path = format!("http://{}/wallet/getAccountStatus", self.endpoint);
-        let resp: RpcResponse<GetAccountStatusRep> = self
-            .agent
-            .clone()
-            .post(&path)
-            .send_json(ureq::json!(req))?
-            .into_json()?;
-        Ok(resp.data)
+        let resp = self.agent.clone().post(&path).send_json(&req);
+        self.handle_response(resp)
     }
 
-    pub async fn get_latest_block(&self) -> Result<GetLatestBlockRep> {
+    pub async fn get_latest_block(&self) -> Result<GetLatestBlockRep, RpcError> {
         let path = format!("http://{}/chain/getChainInfo", self.endpoint);
-        let resp: RpcResponse<GetLatestBlockRep> =
-            self.agent.clone().get(&path).call()?.into_json()?;
-        Ok(resp.data)
+        let resp = self.agent.clone().get(&path).call();
+        self.handle_response(resp)
     }
 }
--- src/rpc_handler/mod.rs
+++ src/rpc_handler/mod.rs
@@ -2,3 +2,12 @@ pub mod abi;
 mod handler;
 
 pub use handler::*;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct RpcError {
+    pub code: String,
+    pub status: u16,
+    pub message: String,
+}
