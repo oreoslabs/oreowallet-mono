9d5f6dc035c5e891505cc9066795d6a60dccd6f2
lovedret <lovedret@6block.com>
2024-06-14 00:07:58 +0800

    Feat: handle blocks already saved

--- crates/chain_loader/src/lib.rs
+++ crates/chain_loader/src/lib.rs
@@ -1,11 +1,19 @@
 use constants::{LOCAL_BLOCKS_CHECKPOINT, PRIMARY_BATCH};
 use db_handler::{DBHandler, DBTransaction, InnerBlock, PgHandler};
 use networking::{rpc_abi::RpcBlock, rpc_handler::RpcHandler};
+use tracing::info;
 use utils::blocks_range;
 
 pub async fn load_checkpoint(rpc_node: String, db_handler: PgHandler) -> anyhow::Result<()> {
     let rpc_handler = RpcHandler::new(rpc_node);
     for group in blocks_range(1..LOCAL_BLOCKS_CHECKPOINT + 1, PRIMARY_BATCH) {
+        if db_handler
+            .get_blocks(group.end as i64, group.end as i64)
+            .await
+            .is_ok()
+        {
+            continue;
+        }
         let results = rpc_handler.get_blocks(group.start, group.end).unwrap();
         let blocks: Vec<RpcBlock> = results
             .data
@@ -28,6 +36,10 @@ pub async fn load_checkpoint(rpc_node: String, db_handler: PgHandler) -> anyhow:
                     .collect(),
             })
             .collect();
+        info!(
+            "save blocks from {} to {} in local db",
+            group.start, group.end
+        );
         let _ = db_handler.save_blocks(inner_blocks).await;
     }
     Ok(())
