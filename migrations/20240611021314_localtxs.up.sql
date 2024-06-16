aa2b6da01a7720e8d52a2d7dee550fd55472e57c
lovedret <lovedret@6block.com>
2024-06-16 11:44:53 +0800

    DBUpgrade: add block hash in txs table

--- migrations/20240611021314_localtxs.up.sql
+++ migrations/20240611021314_localtxs.up.sql
@@ -1,6 +1,7 @@
 -- Add up migration script here
 CREATE TABLE wallet.txs (
     hash CHAR(64) NOT NULL,
+    block_hash CHAR(64) NOT NULL,
     serialized_notes text [],
     CONSTRAINT txt_pkey PRIMARY KEY (hash)
 );
