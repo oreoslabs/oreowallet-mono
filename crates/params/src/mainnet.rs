46d8a28ab5e1cbbb1894bc086c692934f18fc765
lovedret <lovedret@6block.com>
2025-01-29 19:01:08 +0800

    Update mainnet local db checkpoint

--- crates/params/src/mainnet.rs
+++ crates/params/src/mainnet.rs
@@ -28,7 +28,7 @@ impl Network for Mainnet {
 
     const RESCHEDULING_DURATION: std::time::Duration = std::time::Duration::from_secs(30);
 
-    const LOCAL_BLOCKS_CHECKPOINT: u64 = 922_500;
+    const LOCAL_BLOCKS_CHECKPOINT: u64 = 939_400;
 
     const SET_ACCOUNT_LIMIT: usize = 20;
 }
